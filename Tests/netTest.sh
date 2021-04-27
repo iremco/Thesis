856ea27d064b976a791b1cf4b602dc6b14d03876
Irem Coskun <>
2021-04-27 20:38:09 +0200

    paths are updated for shell file

--- Tests/netTest.sh
+++ Tests/netTest.sh
@@ -3,10 +3,10 @@
 
 pip install -r /home/students/coskun/netImplementations/spiralnet/requirement.txt
 
-python ./home/students/coskun/netImplementations/spiralnet/train.py --path=./home/students/coskun/netImplementations/spiralnet/checkpoints/ImagineGAN/celeba/
+python /home/students/coskun/netImplementations/spiralnet/train.py --path=/home/students/coskun/netImplementations/spiralnet/checkpoints/ImagineGAN/celeba/
 
-python ./home/students/coskun/netImplementations/spiralnet/test.py --path=./home/students/coskun/netImplementations/spiralnet/checkpoints/ImagineGAN/celeba/
+python /home/students/coskun/netImplementations/spiralnet/test.py --path=/home/students/coskun/netImplementations/spiralnet/checkpoints/ImagineGAN/celeba/
 
-python ./home/students/coskun/netImplementations/spiralnet/train.py --path=./home/students/coskun/netImplementations/spiralnet/checkpoints/SliceGAN/celeba/
+python /home/students/coskun/netImplementations/spiralnet/train.py --path=/home/students/coskun/netImplementations/spiralnet/checkpoints/SliceGAN/celeba/
 
-python ./home/students/coskun/netImplementations/spiralnet/test.py --path=./home/students/coskun/netImplementations/spiralnet//checkpoints/SliceGAN/celeba/
+python /home/students/coskun/netImplementations/spiralnet/test.py --path=/home/students/coskun/netImplementations/spiralnet//checkpoints/SliceGAN/celeba/
