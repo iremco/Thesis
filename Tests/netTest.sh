75352b57d4cca6d4f849d1727cb77747bfda78b8
Irem Coskun <>
2021-04-27 05:01:06 +0200

    test sh is added

---
+++ Tests/netTest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+pip install -r /home/students/coskun/netImplementations/spiralnet/requirement.txt
+
+python ./home/students/coskun/netImplementations/spiralnet/train.py --path=./home/students/coskun/netImplementations/spiralnet/checkpoints/ImagineGAN/celeba/
+
+python ./home/students/coskun/netImplementations/spiralnet/test.py --path=./home/students/coskun/netImplementations/spiralnet/checkpoints/ImagineGAN/celeba/
+
+python ./home/students/coskun/netImplementations/spiralnet/train.py --path=./home/students/coskun/netImplementations/spiralnet/checkpoints/SliceGAN/celeba/
+
+python ./home/students/coskun/netImplementations/spiralnet/test.py --path=./home/students/coskun/netImplementations/spiralnet//checkpoints/SliceGAN/celeba/
