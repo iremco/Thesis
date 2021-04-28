ce4b06474dc81763db2bb1c9a419ddadb08177a7
Irem Coskun <>
2021-04-28 19:11:57 +0200

    cluster job updates

--- Tests/netTest.sh
+++ Tests/netTest.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 
-pip install -r /home/students/coskun/netImplementations/spiralnet/requirement.txt
+/work/scratch/coskun/miniconda3/bin/python /home/students/coskun/netImplementations/spiralnet/train.py --path=/home/students/coskun/netImplementations/spiralnet/checkpoints/ImagineGAN/celeba/
 
-python /home/students/coskun/netImplementations/spiralnet/train.py --path=/home/students/coskun/netImplementations/spiralnet/checkpoints/ImagineGAN/celeba/
+/work/scratch/coskun/miniconda3/bin/python /home/students/coskun/netImplementations/spiralnet/test.py --path=/home/students/coskun/netImplementations/spiralnet/checkpoints/ImagineGAN/celeba/
 
-python /home/students/coskun/netImplementations/spiralnet/test.py --path=/home/students/coskun/netImplementations/spiralnet/checkpoints/ImagineGAN/celeba/
+/work/scratch/coskun/miniconda3/bin/python /home/students/coskun/netImplementations/spiralnet/train.py --path=/home/students/coskun/netImplementations/spiralnet/checkpoints/SliceGAN/celeba/
 
-python /home/students/coskun/netImplementations/spiralnet/train.py --path=/home/students/coskun/netImplementations/spiralnet/checkpoints/SliceGAN/celeba/
-
-python /home/students/coskun/netImplementations/spiralnet/test.py --path=/home/students/coskun/netImplementations/spiralnet//checkpoints/SliceGAN/celeba/
+/work/scratch/coskun/miniconda3/bin/python /home/students/coskun/netImplementations/spiralnet/test.py --path=/home/students/coskun/netImplementations/spiralnet/checkpoints/SliceGAN/celeba/
